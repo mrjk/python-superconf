3bc2137b92477ad54c6e7f451aa963da4dcbcd57
mrjk <mrjk.78@gmail.com>
2025-01-02 19:45:47 -0500

    tests: fix run examples

--- run_examples.sh
+++ run_examples.sh
@@ -1,13 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 set -euo pipefail
 
-cd examples
+main ()
+{
+  cd examples
+  set -e
 
-python example_104.py --debug true &&
-  python example_105.py --debug true &&
-  python example_106.py --debug true &&
-  python example_107.py --debug true &&
-  python example_108.py --debug true
+  python example_104.py --debug true &&
+    python example_105.py --debug true &&
+    python example_106.py --debug true &&
+    python example_107.py --debug true &&
+    python example_108.py --debug true
+  rc=$?
 
-echo "All tests OK"
+  if [ "$rc" -eq 0 ]; then
+    echo "All tests OK"
+  else
+    echo "All tests FAILED"
+    return 1
+  fi
+}
+
+main
