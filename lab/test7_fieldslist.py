5df6dda3bb902aa1aca48581897e02e055723979
mrjk <mrjk.78@gmail.com>
2025-01-15 03:40:16 -0500

    add: tests for fieldlist

---
+++ lab/test7_fieldslist.py
@@ -0,0 +1,69 @@
+import sys
+from pprint import pprint
+
+import pytest
+
+import superconf.exceptions
+from superconf.common import DEFAULT, FAIL, NOT_SET
+from superconf.configuration import (
+    Configuration,
+    ConfigurationDict,
+    ConfigurationList,
+    Environment,
+)
+from superconf.fields import (
+
+    Field,
+    FieldBool,
+    FieldConf,
+    FieldDict,
+    FieldFloat,
+    FieldInt,
+    FieldList,
+    FieldOption,
+    FieldString,
+    FieldTuple,
+)
+# from superconf.configuration_dev import ConfigurationList
+
+from superconf.loaders import Dict
+
+# This test explore the nested use cases, WITH defaults
+
+
+# Test extra Fields Dict
+# =============================
+
+print("\n\n================ List Fields ===========\n\n")
+
+
+class AppList(ConfigurationList):
+    "Tests types"
+
+    class Meta:
+        # Will fail on undefined casted values if true, alow NOT_SET
+        strict_cast = False
+        default = [
+            {"key1": "item1"},
+            {"key2": "item2"},
+            {"key3": "item3"},
+        ]
+        children_class = ConfigurationDict
+
+app1 = AppList()
+
+EXPECTED = [{'key1': 'item1'}, {'key2': 'item2'}, {'key3': 'item3'}]
+
+
+# pprint(app1.__dict__)
+
+# pprint(app1.get_values())
+
+
+# pprint(app1[1].get_values())
+
+
+assert app1.get_values() == EXPECTED
+
+
+print("All tests O WIPK")
