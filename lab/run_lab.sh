e47b0954b029ecc5d77adc963d6ac43af6cb6186
mrjk <mrjk.78@gmail.com>
2025-01-19 04:34:55 -0500

    test: update script to report all failed test

--- lab/run_lab.sh
+++ lab/run_lab.sh
@@ -5,11 +5,32 @@ lab_dir="$prj_dir/lab"
 
 set -eu
 
-for i in "$lab_dir"/test[0-9]*; do 
-  echo -e "\n\n\n\nRun $i"
-  python $i || { echo "TEST FAILED on $i"; exit 1 ; }
-  echo "Tests are ok on: $i"
-done
+run_tests ()
+{
+  local failed=0
+  local list=""
+
+  for i in "$lab_dir"/test[0-9]*; do
+    echo -e "\n\n\n\nRun $i"
+    if python $i ; then
+      echo "OK: Passed $i"
+    else
+      echo "KO: Failed $i"
+      failed=$(( failed + 1))
+      list="${list} $i"
+    fi
+
+  done
+
+  if [[ "$failed" -ne 0 ]]; then
+    echo
+    echo
+    echo "KO: FAILED $failed times: $list"
+    return 1
+  fi
+}
+
+run_tests
 
 echo
 echo
