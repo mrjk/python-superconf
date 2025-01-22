bc8caa33130effb6b58ecaf769e118c1c81822e1
mrjk <mrjk.78@gmail.com>
2025-01-22 12:37:41 -0500

    fix: _NoneType implemententation

--- superconf/common.py
+++ superconf/common.py
@@ -1,5 +1,6 @@
 "Common code"
 
+# pylint: disable=too-few-public-methods
 
 import json
 import logging
@@ -9,7 +10,7 @@ import yaml
 log = logging.getLogger(__name__)
 
 
-class NotSet(str):
+class _NoneType:
     """
     A special type that behaves as a replacement for None.
     We have to put a new default value to know if a variable has been set by
@@ -20,27 +21,40 @@ class NotSet(str):
 
     def repr(self):
         "Return string representation"
+        return "<NONE_TYPE>"
+
+    def __str__(self):
+        return self.repr()
+
+    def __repr__(self):
+        return self.repr()
+
+    def __bool__(self):
+        return False
+
+
+class NotSet(_NoneType):
+    "Represent an unset arg"
+
+    def repr(self):
         return "<NOT_SET>"
 
-    __str__ = repr
-    __repr__ = repr
 
-
-class UnSetArg(NotSet):
+class UnSetArg(_NoneType):
     "Represent an unset arg"
 
     def repr(self):
         return "<UNSET_ARG>"
 
 
-class Failure(NotSet):
+class Failure(_NoneType):
     "Represent a failure"
 
     def repr(self):
         return "<FAILURE>"
 
 
-class Default(NotSet):
+class Default(_NoneType):
     "Represent a default"
 
     def repr(self):
@@ -52,6 +66,12 @@ UNSET_ARG = UnSetArg()
 FAIL = UnSetArg()
 DEFAULT = Default()
 assert UNSET_ARG is not NOT_SET
+assert NOT_SET is not UNSET_ARG
+assert not isinstance(UNSET_ARG, str)
+assert not isinstance(NOT_SET, str)
+print((NOT_SET or "INVALID"))
+assert (NOT_SET or "INVALID") == "INVALID"
+
 # assert UNSET_ARG is not NOT_SET
 # assert UNSET_ARG == NOT_SET
 
