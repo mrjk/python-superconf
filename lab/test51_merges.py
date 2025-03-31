eed115987e221c4726b173504de65bbd48c6b9f3
mrjk <mrjk.78@gmail.com>
2025-03-31 14:45:45 -0400

    add: test lab

---
+++ lab/test51_merges.py
@@ -0,0 +1,121 @@
+# pylint: skip-file
+
+import sys
+from pprint import pprint
+
+import pytest
+
+import superconf.exceptions
+from superconf.common import DEFAULT, FAIL, NOT_SET
+from superconf.configuration import (
+    ConfigurationObj,
+    ConfigurationDict,
+    ConfigurationList,
+)
+from superconf.fields import (
+    Field,
+    FieldBool,
+    FieldConf,
+    FieldDict,
+    FieldFloat,
+    FieldInt,
+    FieldList,
+    FieldOption,
+    FieldString,
+    FieldTuple,
+)
+
+# from superconf.loaders import Dict
+
+# from superconf.configuration_dev import ConfigurationList
+
+
+# This test explore the nested use cases, WITH defaults
+
+
+# Test extra Fields Dict
+# =============================
+
+
+class AppConfig(ConfigurationObj):
+    "Tests types"
+
+    class Meta:
+        # Will fail on undefined casted values if true, alow NOT_SET
+        strict_cast = False
+        # default = [
+        #     {"key1": "item1"},
+        #     {"key2": "item2"},
+        #     {"key3": "item3"},
+        # ]
+        # children_class = ConfigurationDict
+
+    name = FieldString(default="default_toto")
+    enabled = FieldBool(default=True)
+    count = FieldInt(default=999)
+
+
+class AppDict(ConfigurationDict):
+    "Tests types"
+
+    class Meta:
+        # Will fail on undefined casted values if true, alow NOT_SET
+        strict_cast = False
+        children_class = AppConfig
+
+
+def test_merge_for_configuration():
+
+    # Test1: Test merge for ConfigurationObj
+    app1_val = {"name": "toto", "count": 25}
+    app2_val = {"name": "titi", "enabled": False}
+
+    app1 = AppConfig(value=app1_val)
+    app2 = AppConfig(value=app2_val)
+
+    # pprint(app1.get_values())
+    # pprint(app2.get_values())
+
+    # test merge for dicts
+    out = app1.merge(app2)
+    assert isinstance(out, AppConfig)
+
+    EXPECTED = {"count": 10, "enabled": False, "name": "titi"}
+    # pprint(out.get_values())
+    assert out.get_values() == EXPECTED
+
+
+def test_merge_for_configuration_dict():
+    # Test2: Test merge for ConfigurationDict
+    app1_val = {
+        "app1": {"name": "toto", "count": 25},
+        # "app2": {"name": "titi", "enabled": False},
+    }
+    app2_val = {
+        "app1": {"name": "tata"},
+        # "app3": {"name": "tutu", "enabled": True},
+    }
+
+    print("PREPARE THINGS")
+    appdict_1 = AppDict(value=app1_val)
+    appdict_2 = AppDict(value=app2_val)
+
+    print("MERGE THINGS")
+    out = appdict_1.merge(appdict_2)
+    assert isinstance(out, AppDict)
+
+    print("\n\n\nCHECK RESULT")
+    pprint(out.get_values())
+    EXPECTED = {
+        "app1": {"count": 25, "enabled": True, "name": "tata"},
+        "app2": {"count": 10, "enabled": False, "name": "titi"},
+        "app3": {"count": 10, "enabled": True, "name": "tutu"},
+    }
+
+    assert out.get_values() == EXPECTED
+
+
+# test_merge_for_configuration()
+# test_merge_for_configuration_dict()
+
+print("All tests OK")
---
+++ lab/test52_shortform.py
@@ -0,0 +1,98 @@
+from pprint import pprint
+import logging
+from superconf import ConfigurationObj, ConfigurationDict, FieldString, FieldList, FieldDict, FieldConf
+from superconf.exceptions import InvalidFieldOption
+
+# logging.basicConfig(level=logging.DEBUG)
+
+
+class AppFeature(ConfigurationObj):
+    """Feature object"""
+
+    class Meta:
+        extra_fields = True
+
+    name = FieldString(help="Feature name")
+    group = FieldString(help="Default group", default="__base__")
+
+
+# class AppFeatures(ConfigurationDict):
+#     """App Features Feature"""
+
+#     class Meta:
+#         children_class = AppFeature
+
+
+class App(ConfigurationObj):
+    """Main App"""
+
+    class Meta:
+        extra_fields = False
+
+    # Application configuration
+    # default_features = FieldList(help="default features")
+    # resource_model = FieldDict(help="resource model")
+
+
+    # This is the long form
+    # features = FieldConf(AppFeatures)
+
+    # This is the short form
+    features = FieldConf(
+        ConfigurationDict,
+        help="THIS IS MY ITEM",
+        children_class=AppFeature,
+        # extra_fields=True,
+    )
+
+
+# Test failure for extra fields, since there is not such option in ConfigurationDict
+try:
+    features = FieldConf(
+        ConfigurationDict,
+        extra_fields=True,
+    )
+    assert False, "Should have raised an excveption"
+except InvalidFieldOption:
+    pass
+
+
+# pprint(ConfigurationDict.__node_config__.__dict__)
+# pprint(ConfigurationObj.__node_config__.__dict__)
+
+
+app_payload = {
+    "features": {
+        "feat1": {
+            "name": "feat1",
+            "group": "group1",
+        },
+        "feat2": {
+            "name": "feat2",
+            "group": "group2",
+            "fake_feat": "TUTU",
+        }
+    }
+
+}
+
+app = App(value=app_payload)
+
+# pprint(app)
+# pprint(app.get_value())
+
+# pprint(app.features)
+# pprint(app.features.get_value())
+# pprint(app.features)
+# pprint(app.features.__dict__)
+
+print("FEATURE HELP", app.features.__node_help__)
+for feature in app.features:
+
+    print("FEATURE", type(feature), feature)
+    assert isinstance(feature, AppFeature)
+    print("FEATURE HELP", feature.__node_help__)
+
+
+print("All tests OK")
+
